d0ec581ae4c13bc66f921d98c74fe062f12795ba
faizallmaullana <faizalmaulanasekias@gmail.com>
2024-02-15 09:32:39 +0700

    fix: fix database always cloded

--- controller/db-connection.go
+++ controller/db-connection.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"database/sql"
 	"fmt"
-	"sync"
 
 	_ "github.com/lib/pq"
 )
@@ -11,17 +10,16 @@ import (
 var (
 	db          *sql.DB
 	dbInitError error
-	dbOnce      sync.Once
 )
 
 func getDB() (*sql.DB, error) {
-	// Use sync.Once to ensure the initialization is done only once
-	dbOnce.Do(func() {
+	if db == nil {
 		connStr := "user=tanjung password=APWa4n3XiTfx host=ep-green-wave-a1c4bn3h.ap-southeast-1.pg.koyeb.app dbname=koyebdb sslmode=require"
 		db, dbInitError = sql.Open("postgres", connStr)
 		if dbInitError != nil {
 			fmt.Println("Error connecting to database:", dbInitError)
+			return nil, dbInitError
 		}
-	})
+	}
 	return db, dbInitError
 }
